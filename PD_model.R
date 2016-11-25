126a34139b5930494e9c682bfa3dc90c4de0f476
ItsJanul <jh2609@stern.nyu.edu>
2016-11-25 00:24:13 -0500

    latest model

--- PD_model.R
+++ PD_model.R
@@ -6,10 +6,12 @@
 #LIBRARIES-----------------------------------------------------------------
 library(lubridate)
 library(tidyverse)
+library(microbenchmark)
 
-#FUNCTIONS--------------------------------------------------------------------------------------------------------------------------
+#change # of rows to print on tibble
+options(tibble.print_max = 1000, tibble.print_min = 200)
 
-
+#FUNCTIONS-----------------------------------------------------------------
 
 #FUNCTION TO CALCULATE AUC ROC FOR 2 DIFFERENT SUBSET OF A SINGLE DATASET
 subROC <- function(x,	split.val,	split.on,	score,	outcome)
@@ -38,22 +40,27 @@ subROC <- function(x,	split.val,	split.on,	score,	outcome)
   return(ret)
 }
 
-
-#FUNCTION TO CREATE CALIBRATION CURVE MAPPING TO A DEFAULT RATE
-estimateCalibCurve	<-	function(x,outcome,k=25,plot=T,spar=0.5)	
+#FUNCTION TO CALCULATE DENSITY ESTIMATES  
+densityMap	<-	function(x,outcome,k=25,plot=T,xaxis="breaks",spar=0.5)	
 {	
-  breaks<-(0:k)/k	
-  qs<-quantile(x,breaks)	
-  buckets<-cut(x,qs)
+  df =data.frame(x, outcome)
+  df<-na.omit(df)
   
-  raw.curve<-tapply(X=outcome,IND=buckets,FUN=mean)	
+  breaks<-(0:k)/k	
+  qs<-quantile(df$x, breaks)	
+  buckets<-cut(df$x, qs)
+  
+  raw.curve<-tapply(X=df$outcome , IND=buckets , FUN=mean)	
   curve<-smooth.spline(breaks[-1],raw.curve,spar=spar)	
   pred<-predict(curve,breaks[-1])
   
   if(plot)	
   {	
-    plot(breaks[-1],raw.curve)	
-    lines(pred,col="red",lwd=3)	
+    plot(breaks[-1],raw.curve,
+         main="Density Plot",
+         xlab=xaxis,
+         ylab="PD")
+    lines(pred,col="red",lwd=3)
   }	
   
   baseline<-mean(outcome)	
@@ -64,42 +71,77 @@ estimateCalibCurve	<-	function(x,outcome,k=25,plot=T,spar=0.5)
   return(list(baseline=baseline,map=map))	
 }
 
-
-#FUNCTION TO USE CALIBRATION CURVE TO MAP A VARIABLE TO A DEFAULT RATE
-applyCalibCurve	<-	function(x,	map,	baseline=NULL)	
+#FUNCTION TO USE DENSITY ESTIMATE TO MAP A VARIABLE TO A PD
+applyDensityMap	<-	function(x,	map)	
 {
-  cutoff<-map$map$cutoff	
-  PD<-map$map$PD	
-  len<-length(cutoff)
-  lb<-sum(cutoff<x)	
-  if(lb==0)	pd<-PD[1]	
-  else	if(lb>=len)	pd<-PD[len]
-  else	#interpolate	
-  {	
-    ub<-lb+1	
-    num<-x-cutoff[lb]	
-    den<-cutoff[ub]-cutoff[lb]	
-    wt.ub<-num/den	
-    wt.lb<-1-wt.ub	
-    pd<-wt.lb*PD[lb]+wt.ub*PD[ub]	
-  }	
+  output <- vector("double", length(x)) 
+  cutoff <- map$map$cutoff
+  PD <- map$map$PD
+  len <- length(cutoff)
   
-  if(is.null(baseline))	p.star<-pd	
-  else	
-  {	
-    piS<-map$baseline	
-    piT<-baseline	
-    num<-pd-(pd*piS)	
-    den<-piS-(pd*piS)+(pd*piT)-(piS*piT)	
-    p.star<-piT*num/den	
-  }	
-  return(p.star)	
+  for (i in seq_along(x)) {
+    if (is.na(x[i]))
+      pd <- NA
+    else{
+      lb <- sum(cutoff < x[i])
+      if (lb == 0)
+        pd <- PD[1]
+      else	if (lb >= len)
+        pd <- PD[len]
+      else
+      {
+        ub <- lb + 1
+        num <- x[i] - cutoff[lb]
+        den <- cutoff[ub] - cutoff[lb]
+        wt.ub <- num / den
+        wt.lb <- 1 - wt.ub
+        pd<-wt.lb*PD[lb]+wt.ub*PD[ub]	
+      }
+    }
+    #if (pd>0 || is.na(pd)) output[i]<- pd
+    #else output[i]<- 0
+    output[i]<- pd
+  }
+  return(output)
+}
+
+#FUNCTION TO TRANSFORM VARIABLE TO PD
+transformVar <- function(x, outcome, k=25, plot=T, xaxis="breaks")
+{
+  fit<- densityMap(x, outcome, k=k, xaxis=xaxis)
+  output<- applyDensityMap(x, fit)
+  return(list(baseline=fit[[1]], map=fit[[2]], transformedVar=output, buckets=k))
+}
+
+#AUTO-FIT DENSITY ESTIMATE
+autoCal<- function(x, outcome, k=25, plot=T, xaxis="breaks")
+{
+  output<-transformVar(x, outcome, k = k, plot = plot, xaxis = xaxis)
+  
+  while(summary(output[[3]])[[1]]<0){
+    k=k+1
+    output<-transformVar(x, outcome, k = k, plot = plot, xaxis = xaxis)
+  }
+  return(output)
+}
+
+#FUNCTION FOR DEFAULT TIMELINE
+defaultTimeline <-function (df)
+    #need to send dates as Default.Date and bank ids as ID
+{
+    output <- df %>%
+      filter(!is.na(Default.Date)) %>%
+      group_by(yearx= year(Default.Date)) %>%
+      summarise(number_banks = n_distinct(ID)) %>%
+      ggplot(., aes(x = yearx, y = number_banks)) + 
+          geom_area() + 
+          ggtitle("Bank Defaults") + 
+          labs(x = "Year", y = "Total Defaults")
+    return(output)
 }
 
 
-
-
-#RUNNING CODE--------------------------------------------------------------------------------------------------------------------
+#RUNNING CODE--------------------------------------------------------------
 
 #creating a tibble called bankdata
 bankdata<-as_data_frame(bankdata.in.new)
@@ -114,45 +156,43 @@ bankdata<- bankdata %>%
                                   Default.Date-repdte.adjust)) %>%
   mutate(default.flag= ifelse (is.na(days.to.default), 0, ifelse(days.to.default >0 & days.to.default <= 365.25, 1, 0)))
 
-#DATA VIZ FOR ANALYSIS OF VARIABLES------
 
-#Measures of profitability: 
+#Setting training set
+set.seed(1);
+training.sample.naive<-runif(dim(bankdata)[1])<0.70
+training.sample.date<-(year(bankdata$repdte.adjust)<2005)
+
+
+#TESTING DIFFERENT VARIABLES
+
+#MEASURE OF PROFITABILITY: 
 #Pre-tax return on assets- "roaptx" 
-plot(bankdata$roaptx, bankdata$default.flag, pch = 16, xlab = "roaptx", ylab = "PD")
-hist(bankdata$roaptx)
-hist(log(bankdata$roaptx)) #normal distributed
+profit.fit<-autoCal(bankdata$roaptx, bankdata$default.flag, plot = T, xaxis = "roaptx")
 
-#Net operating income over assets- "noijy"
-plot(bankdata$noijy, bankdata$default.flag, pch = 16, xlab = "noijy", ylab = "PD")
-hist(bankdata$noij)
-hist(log(bankdata$noij)) #normal distributed
+#MEASURE OF LEVERAGE: 
+#Total asset over total liability- "asset/liability"
+bankdata$lev<-bankdata$asset/(bankdata$liab+1)
+leverage.fit<-densityMap(bankdata$lev, bankdata$default.flag, xaxis = "leverage")
 
-#Measures of leverage: 
-#Total debt over total assets- "liab/asset"
-plot(bankdata$liab/bankdata$asset, 
-     bankdata$default.flag, pch = 16, xlab = "liab/asset", ylab = "PD")
-
-#Total debt over Net operating income- "liab/idpretx"
-plot(bankdata$liab/(bankdata$idpretx+1), 
-     bankdata$default.flag, pch = 16, xlab = "debt/NOPAT", ylab = "PD")
-
-#Average equity over total debt- "eq5/liab" Net loans&leases over core deposits- "idlncorr"
-plot(bankdata$eq5/bankdata$liab, 
-     bankdata$default.flag, pch = 16, xlab = "avg_equity/total_debt", ylab = "PD")
-
-#Common tier 1 equity over risk-adjusted assets- "rbct1cer"
-plot(bankdata$rbct1cer, bankdata$default.flag, pch = 16, xlab = "equity/risk-adj assets", ylab = "PD")
-
-#Measures of debt coverage: 
+#MEASURES OF DEBT COVERAGE: 
 #Pre-tax Net operating income over interest expense- "idpretx/eintexp"
-plot(bankdata$idpretx/(bankdata$eintexp+1), 
-     bankdata$default.flag, pch = 16, xlab = "NOPAT/Interets_Exp", ylab = "PD")
+bankdata$noi_int<-bankdata$idpretx/(bankdata$eintexp+1)
 
-#Net income minus cash dividends over interest expense- "(netinc-eqcdiv)/eintexp"
+test1<-transformVar(bankdata$noi_int, bankdata$default.flag, k=50, xaxis = "debt_coverage")
+summary(test1)
 
-#Measure of liquidity: 
+#MEASURE OF LIQUIDITY: 
 #Tier 1 Capital over Total risk-weight assets- "rbc1rwaj"
+liquidity.fit<- densityMap(bankdata$rbc1rwaj, bankdata$default.flag, xaxis = "liquidity")
 
-#Measures of size: Adjusted average assets- "avassetj" 
+#MEASURE OF SIZE: 
 #Average assets- "asset5"
+size.fit<- densityMap(bankdata$asset5, bankdata$default.flag, xaxis = "size")
+
+test.fit<-transformVar2(bankdata$noi_int, bankdata$default.flag, k=k,plot = F, xaxis = "debt_coverage")
+
+microbenchmark(var1= transformVar(bankdata$rbc1rwaj, bankdata$default.flag, k=k, xaxis = "debt_coverage"),
+               var2=transformVar2(bankdata$rbc1rwaj, bankdata$default.flag, k=k,plot = F, xaxis = "debt_coverage"),
+               times = 5)
+
 
